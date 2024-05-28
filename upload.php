3941313848a697ad12f276682d989d6d449a5bab
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-05-27 21:01:27 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,3 +1,11 @@
 <?php
 $pastaDestino = "/uploads/";
-var_dump($_POST);
+var_dump($_FILES);
+var_dump($_FILES['arquivo']['size']);
+if ($_FILES['arquivo']['size'] > 2000000){
+    echo "O tamanho do arquivo é maior que o limite permitido.";
+    die();
+}
+var_dump($_FILES['arquivo']['name']);
+
+var_dump(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
