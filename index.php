58f34db5142ab5d3403437aa90607e01e3d15f4f
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-06-06 15:47:11 -0300

    aula dia 06

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$conexao= mysqli_connect("localhost", "root", "", "upload-arquivos");
+$sql= "SELECT * FROM arquivo";
+$resultado= mysqli_query($conexao, $sql);
+if($resultado != false){
+    $arquivos = mysqli_fetch_all($resultado, MYSQLI_BOTH);
+}
+    else{
+        echo "Erro ao executar o comando sql.";
+        die();
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +23,22 @@
     <form action = "upload.php" method = "post" enctype= "multipart/form-data">
         Adicione o arquivo:
         <input type="file" name="arquivo"><br><br>
-        <input type="submit" value="Fazer Upload" name="submit">
+        <input type="submit" value="Fazer Upload" name="submit"><br><br>
+    <table>
+        <tr> 
+            <th> Nome do Arquivo </th>
+            <th colspan = "2"> Opções </td>
+        </tr>
+    <thead>
+    <tbody> 
+        <?php
+        foreach($arquivos as $arquivos){
+            echo "<tr> <td>".$arquivos['nome_arquivo']."</td>";
+            echo "<td> <a href ='alterar.php?nome_arquivo=".$arquivos['nome_arquivo']."'> Alterar </td>";
+            echo "<td> <button> excluir </button> </td> </tr>";
+        }
+        ?>
+    </tbody>
+    </table>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -6,8 +6,6 @@ if ($_FILES['arquivo']['size'] > 2000000){
     die();
 }
 
-
-
 $extensao= strtolower(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
 
 if($extensao != "png" && $extensao != "jpg" && $extensao != "jpeg" && $extensao != "gif" && $extensao != "jfif" && $extensao != "svg"){
@@ -20,11 +18,37 @@ if(getimagesize($_FILES['arquivo']['tmp_name']) === false){
 }
 
 $nomeArquivo = uniqid();
-
 $fezUpload = move_uploaded_file($_FILES['arquivo']['tmp_name'], __DIR__ . $pastaDestino . $nomeArquivo . ".". $extensao);
 if($fezUpload == true){
+
+    $conexao = mysqli_connect("localhost", "root", "", "upload-arquivos");
+    $sql= "INSERT INTO arquivo (nome_arquivo) VALUES ('$nomeArquivo.$extensao')";
+    $resultado = mysqli_query($conexao, $sql);
+    if($resultado != false)
+    {
+        if(isset($_POST['nome_arquivo']) ){
+            $apagou= unlink( __DIR__ . $pastaDestino . $_POST['nome_arquivo']);
+            if($apagou == true){
+                $sql= "DELETE FROM arquivo WHERE nome_arquivo='".$_POST['nome_arquivo']."'";
+                
+                $resultado2= mysqli_query($conexao, $sql);
+                if($resultado2 == false){
+                    echo "Erro ao apagar o arquivo do banco de dados";
+                    die();
+                }
+            }else{
+                echo "Erro ao apagar o arquivo antigo.";
+                die();
+            }
+        }
     header("location: index.php");
+    } 
+    else 
+    {
+        echo "Erro ao registrar o arquivo no banco";
+    }
+
 }
-else{
+else {
     echo "Erro ao mover o arquivo";
 }
